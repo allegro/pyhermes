6cb25182a2dd93ae261484bc3b31531da84ac914
Damian Blejwas <Damian Blejwas>
2022-09-02 16:01:05 +0200

    Django 3.2 compatibility

--- pyhermes/__init__.py
+++ pyhermes/__init__.py
@@ -1,7 +1,7 @@
 from pyhermes.decorators import publisher, subscriber
 from pyhermes.publishing import publish
 
-__version__ = '0.4.3'
+__version__ = '0.5.0'
 
 __all__ = [
     'publish',
--- pyhermes/apps/django/config.py
+++ pyhermes/apps/django/config.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PyhermesConfig(AppConfig):
     name = 'pyhermes.apps.django'
-    label = 'pyhermes.django'
+    label = 'pyhermes_django'
