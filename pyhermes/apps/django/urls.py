3bdf35d7345e52032134b5efa3d0bd005224d3ee
Aleksander Więckowski <aleksander.wieckowski@gmail.com>
2023-02-13 10:44:36 +0100

    Add compatibility with Django 4.1 (#27)

--- pyhermes/apps/django/urls.py
+++ pyhermes/apps/django/urls.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 import django
-from django.conf.urls import url
 
 from pyhermes.apps.django.views import subscriber_view
 
-urlpatterns = [
-    url(
-        r'^events/(?P<subscriber_name>[a-zA-Z0-9_\.-]+)/$',
-        subscriber_view,
-        name='hermes-event-subscriber',
-    ),
-]
 
-if django.VERSION <= (1, 7):
-    from django.conf.urls import patterns
-    urlpatterns = patterns('', *urlpatterns)
+if django.VERSION < (2, 0, 0):
+    from django.conf.urls import url
+    urlpatterns = [
+        url(
+            r'^events/(?P<subscriber_name>[a-zA-Z0-9_\.-]+)/$',
+            subscriber_view,
+            name='hermes-event-subscriber',
+        ),
+    ]
+
+    if django.VERSION <= (1, 7):
+        from django.conf.urls import patterns
+        urlpatterns = patterns('', *urlpatterns)
+
+else:
+    from django.urls import re_path
+    urlpatterns = [
+        re_path(
+            r'^events/(?P<subscriber_name>[a-zA-Z0-9_\.-]+)/$',
+            subscriber_view,
+            name='hermes-event-subscriber',
+        ),
+    ]
